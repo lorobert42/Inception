350cb218999a8886ddd8c470562f69a842064ef5
lorobert <lorobert@student.42lausanne.ch>
2023-07-14 20:56:28 +0200

    mariadb & wordpress composed

--- srcs/requirements/mariadb/conf/mariadb.sh
+++ srcs/requirements/mariadb/conf/mariadb.sh
@@ -3,5 +3,5 @@ mariadb --execute="CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mariadb --execute="CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mariadb --execute="GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mariadb --execute="FLUSH PRIVILEGES;"
-service mariadb stop
+mysqladmin shutdown
 mysqld_safe
--- srcs/requirements/wordpress/conf/wordpress.sh
+++ srcs/requirements/wordpress/conf/wordpress.sh
@@ -1,15 +1,19 @@
 wp-cli.phar core download \
-    --path=/var/www/wordpress
+    --path=/var/www/wordpress \
+    --allow-root
 wp-cli.phar config create \
     --dbname=$SQL_DATABASE \
     --dbuser=$SQL_USER \
     --dbpass=$SQL_PASSWORD \
     --dbhost=mariadb:3306 \
-    --path=/var/www/wordpress
+    --path=/var/www/wordpress \
+    --allow-root
 wp-cli.phar core install \
     --url=lorobert.42.fr \
     --title=lorobert \
     --admin_user=$WP_USER \
     --admin_password=$WP_PASSWORD \
     --admin_email=wordpress@lorobert.42.fr \
-    --path=/var/www/wordpress
+    --path=/var/www/wordpress \
+    --allow-root
+php-fpm8.2 --nodaemonize
